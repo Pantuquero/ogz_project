74f27ff9383b8faf1fa1044ef5e0934e0820d029
pantuquero <pantuquero@gmail.com>
2017-05-14 11:05:02 +0200

    v0.2 Added calendar

---
+++ src/java/servlets/Index.java
@@ -0,0 +1,110 @@
+package servlets;
+
+import clases.Usuario;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Pantuquero
+ */
+@WebServlet(name = "Index", urlPatterns = {"/Index"})
+public class Index extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            
+            // Compruebo la sesión, si está inactiva devuelvo al login
+            if(comprobarSesion(request)) {
+                
+                response.sendRedirect("index.jsp");
+            } else {
+                
+                request.setAttribute("mensaje", "Your session has expired.");
+                request.setAttribute("tipo_mensaje","false");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * Comprueba que la sesión está inicializada buscando el usuario.
+     * @param request
+     * @return boolean true -> si la sesión es correcta
+     */
+    private boolean comprobarSesion(HttpServletRequest request){
+        System.out.println("Comprobando usuario...");
+        
+        HttpSession sesion = (HttpSession) request.getSession();
+        Usuario usuario =  (Usuario) sesion.getAttribute("usuario");
+        
+        //System.out.println(usuario.getNombre() + " " + usuario.getEmail() + " " + usuario.getContrasena());
+        
+        if(usuario.getNombre() != null){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
